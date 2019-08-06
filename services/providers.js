11c8be169795fb152827e7d2146abc46595968cb
Marco Antonio Macedo Preciado <marcosiegman01@gmail.com>
2019-08-06 01:16:33 -0500

    Service for providers added

---
+++ services/providers.js
@@ -0,0 +1,12 @@
+const MariaLib = require("../lib/mariadb");
+
+class ProvidersServices {
+  constructor() {
+    this.table = "providers";
+    this.mariadb = new MariaLib();
+  }
+  async getAll() {
+    const providers = await this.mariadb.getAll(this.table);
+    return providers;
+  }
+}
