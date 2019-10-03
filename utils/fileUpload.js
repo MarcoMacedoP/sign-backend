4238a315c0795d3c55947c74fdaa1011dafcd708
Marco Macedo <marcosiegman01@gmail.com>
2019-10-03 11:58:18 -0500

    added upload files util and multer lib

---
+++ utils/fileUpload.js
@@ -0,0 +1,10 @@
+const multer = require("multer");
+
+const fileUpload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => cb(null, "public/static/uploads"),
+    filename: (req, file, cb) =>
+      cb(null, `${new Date().toISOString}_${file.originalname}`)
+  })
+});
+module.exports = fileUpload;
