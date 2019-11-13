6826a4c34a5bab7d5c4669d0ef0f963fd7fc755e
Marco Macedo <marcosiegman01@gmail.com>
2019-11-12 20:06:40 -0600

    added teams-reminders

---
+++ services/teams/teams-reminders.js
@@ -0,0 +1,34 @@
+const ReminderServices = require("../reminders/reminders");
+const TeamsServices = require("./teams");
+class TeamsRemindersServices {
+  collection() {
+    return new TeamsServices();
+  }
+  addReminderToTeam({ reminderId, teamId }) {
+    const addToSet = {
+      reminders: {
+        reminderId
+      }
+    };
+    return this.collection().updateOne(teamId, addToSet, "$addToSet");
+  }
+  removeReminderOfTeam({ reminderId, teamId }) {
+    const pull = {
+      reminders: {
+        reminderId
+      }
+    };
+    return this.collection().updateOne(teamId, pull, "$pull");
+  }
+  async getRemindersOfATeam({ teamId }) {
+    const remindersServices = new ReminderServices();
+    const { reminders = [] } = await this.collection().getOneById(teamId);
+    //get just the value of the reminders
+    const remindersIds = reminders.map(
+      reminder => reminder.reminderId && reminder.reminderId
+    );
+
+    return remindersServices.findInArray(remindersIds);
+  }
+}
+module.exports = TeamsRemindersServices;
--- services/teams/teams.js
+++ services/teams/teams.js
@@ -1,5 +1,5 @@
 const MongoLib = require("../../lib/mongodb");
-const {ObjectId} = require("mongodb");
+const { ObjectId } = require("mongodb");
 const TeamsUsersServices = require("./teams-users");
 const debug = require("debug")("app:services:teams");
 
@@ -9,7 +9,7 @@ class Teams {
     this.membersCollection = () => new TeamsUsersServices();
   }
 
-  async createOne({teamData, founderId}) {
+  async createOne({ teamData, founderId }) {
     const team = await this.collection().createOne(teamData);
     const members = await this.membersCollection().addUserToTeamAsFounder({
       teamId: team._id,
@@ -22,21 +22,21 @@ class Teams {
     };
   }
 
-  removeById({teamId}) {
+  removeById({ teamId }) {
     return this.collection()
       .removeOneById(teamId)
-      .then(({deletedCount}) => ({deletedCount, teamId}));
+      .then(({ deletedCount }) => ({ deletedCount, teamId }));
   }
 
-  updateOne(teamId, data, extraFilter = {}) {
+  updateOne(teamId, data, extraFilter = {}, operator = "") {
     debug(teamId);
-    const filter = {_id: new ObjectId(teamId), ...extraFilter};
+    const filter = { _id: new ObjectId(teamId), ...extraFilter };
     debug(filter);
-    return this.collection().updateOne(filter, data);
+    return this.collection().updateOne(filter, data, operator);
   }
   getMany(arrayOfIds = []) {
     return this.collection().readAll({
-      _id: {$in: arrayOfIds.map(id => new ObjectId(id))}
+      _id: { $in: arrayOfIds.map(id => new ObjectId(id)) }
     });
   }
   getOne(filter) {
